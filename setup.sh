072d4a48757848d79049dd5e9c2d670693b152e4
Leonid Zagumennov <leonzag997@gmail.com>
2024-10-20 22:49:57 +0300

    upd

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+echo ">>> apt install fail2ban less man nvim ranger curl"
+sudo apt install fail2ban less man nvim ranger curl
+
 echo ">>> apt install zsh zsh-autosuggestions zsh-syntax-highlightinv zsh-common zsh-doc"
-apt install \
-	zsh \
-	zsh-autosuggestions \
+sudo apt install \
+    zsh \
+    zsh-autosuggestions \
     zsh-syntax-highlightinv \
     zsh-common \
     zsh-doc
@@ -14,4 +17,4 @@ sudo apt install build-essential
 echo ">>> install romkatv/p10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'if no p10k in .zshrc, run:'
-echo '  source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+echo '  source ~/powerlevel10k/powerlevel10k.zsh-theme >>~/.zshrc'
